df4ab6863b7f02d9048aef6d03131c4b34396de3
mogwai <feelirabs@gmail.com>
2020-01-09 14:01:55 +0100

    Removed excess card from Pane1

--- src/components/DashboardHome/Pane1.js
+++ src/components/DashboardHome/Pane1.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const Pane1 = ()=> {
     return (
@@ -7,9 +7,6 @@ const Pane1 = ()=> {
         <div id='cards'>
         <div className="Cards">
             <p style={{color:'white', paddingTop:'40px'}}>This month you earned:</p>
-        </div>
-        <div className="Cards2">
-            <p style={{color:'white', paddingTop:'40px'}}>Lifetime earnings:</p>
         </div>
         <div className="Cards2">
             <p style={{color:'white', paddingTop:'40px'}}>Lifetime earnings:</p>
